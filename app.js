957d579a65a0d9bef8e5b4f138fc20a0fe4d7c10
unknown <maxwellcarrascogarxia>
2025-07-21 14:55:36 -0600

    no se q comit es este

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";//trae las variables de entorno
 import cors from "cors";//crea seguridad en el servidor
 
 dotenv.config();//iniciando las variables de entorno
-
+g
 
 //conectar a la base de datos
 mongoose.connect(process.env.urlbd)
